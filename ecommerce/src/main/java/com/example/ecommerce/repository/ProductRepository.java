26af1c7fbdf650f6b07cfd9a41efcf44c3d9b332
Jinal Chandibhamar <jinalc17@outlook.com>
2024-07-26 16:01:02 -0400

    added backend initial code

---
+++ ecommerce/src/main/java/com/example/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.example.ecommerce.repository;
+
+import com.example.ecommerce.model.Product;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, ObjectId> {
+
+    Optional<Product> findProductByName(String name);
+}
---
+++ ecommerce/src/main/java/com/example/ecommerce/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.example.ecommerce.service;
+
+import com.example.ecommerce.repository.ProductRepository;
+import com.example.ecommerce.model.Product;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+    public List<Product> allProducts(){
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> singleProduct(ObjectId id){
+        return productRepository.findById(id);
+    }
+}
